4c19d8a163806654ad93674360768c3c81f700dc
Prischepova <71315681+Prischepova@users.noreply.github.com>
2022-03-21 09:48:36 +0300

    Added a project

---
+++ WorldCinemaDesign/WorldCinemaDesign/MainScreenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainScreenViewController.swift
+//  WorldCinemaDesign
+//
+//  Created by Student on 17.03.2022.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    
+    @IBOutlet weak var lookButton: UIButton!
+    
+    @IBOutlet weak var settingsForUButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurateViews()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func configurateViews() {
+        
+        lookButton.layer.cornerRadius = 4
+        settingsForUButton.layer.cornerRadius = 4
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
